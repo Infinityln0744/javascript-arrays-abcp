1189f72ea540fbd0e8cba5eb7c182f19235af2cd
Helen <nikishina.olena@gmail.com>
2018-01-10 21:54:16 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars
+= ["snickers","hundred grand","kitkat","skittles"];
+
+function addElementToBeginningOfArray() {
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+}
+
+function addElementToEndOfArray(){
+}
+
+function destructivelyAddElementToEndOfArray(){
+}
+
+function accessElementInArray(){
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(){
+}
+
+function removeElementFromBeginningOfArray(){
+}
+
+function destructivelyRemoveElementFromEndOfArray(){
+}
+
+function removeElementFromEndOfArray(){
+}
+
