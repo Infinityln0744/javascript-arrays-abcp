2bfd77978d452d39ac2a81d09049cc94b4b2b3db
Helen <nikishina.olena@gmail.com>
2018-01-10 23:08:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,6 +20,7 @@ function destructivelyAddElementToEndOfArray(array,element){
 }
 
 function accessElementInArray(array,index){
+  return array[index]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array,element){
