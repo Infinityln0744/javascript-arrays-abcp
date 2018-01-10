a6cba6acc0e11880898df0bde3ba8f471455daa7
Helen <nikishina.olena@gmail.com>
2018-01-10 23:09:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,7 @@ function accessElementInArray(array,index){
   return array[index]
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array,element){
+function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
 }
 
